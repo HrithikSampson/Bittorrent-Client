3e5594aa8d23cd06cdf6829a93aab8fb628bade0
Hrithik Sampson <hrithikedwardsampson@gmail.com>
2023-11-25 23:08:03 +0530

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+
 use serde_json;
 use std::env;
+use serde_bencode;
 
 // Available if you need it!
 // use serde_bencode
@@ -7,18 +9,28 @@ use std::env;
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
     // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
-        // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return serde_json::Value::String(string.to_string());
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
-    }
+    println!("{}",encoded_value.to_string());
+    let decoded_value: serde_json::Value = serde_json::to_value(serde_bencode::from_str(&encoded_value).unwrap_or_else(|error|{
+        panic!("Error encoding value to beencode:{}",error);
+    })).unwrap_or_else(|error|{
+        panic!("Error converting bencode to value format:{}",error);
+    });
+    decoded_value
 }
 
+// fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
+//     // If encoded_value starts with a digit, it's a number
+//     if encoded_value.chars().next().unwrap().is_digit(10) {
+//         // Example: "5:hello" -> "hello"
+//         let colon_index = encoded_value.find(':').unwrap();
+//         let number_string = &encoded_value[..colon_index];
+//         let number = number_string.parse::<i64>().unwrap();
+//         let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
+//         return serde_json::Value::String(string.to_string());
+//     } else {
+//         panic!("Unhandled encoded value: {}", encoded_value)
+//     }
+// }
 // Usage: your_bittorrent.sh decode "<encoded_value>"
 fn main() {
     let args: Vec<String> = env::args().collect();
